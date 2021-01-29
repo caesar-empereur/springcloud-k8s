c12a38cd225ba26573a938aa005108210b3e55c6
leon <2944@winnermedical.com>
2021-01-29 09:35:02 +0800

    r5er55675r65

--- service-consumer/src/main/java/com/controller/ServiceConsumerController.java
+++ service-consumer/src/main/java/com/controller/ServiceConsumerController.java
@@ -5,8 +5,6 @@ import com.model.Order;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
